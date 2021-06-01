9b228440f46d410a3807b46b136dd9f3665052a0
Clark Bynum <cbynum@clearblade.com>
2021-06-01 16:02:00 -0500

    add log to globals

--- globals.d.ts
+++ globals.d.ts
@@ -2,4 +2,6 @@
 declare var ClearBlade: CbServer.ClearBladeGlobal;
 declare var ClearBladeAsync: CbServer.ClearBladeAsync;
 declare var MQTT: CbServer.MQTT;
+type Logger = (message?: unknown, ...args: unknown[]) => void;
+declare var log: Logger;
 /* eslint-enable no-var */
