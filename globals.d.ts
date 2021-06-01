be8de367129356792e4f8903409b5a16df285ccf
Clark Bynum <cbynum@clearblade.com>
2021-06-01 15:25:36 -0500

    move variables to their own global file so that they can be referenced separately

---
+++ globals.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-var */
+declare var ClearBlade: CbServer.ClearBladeGlobal;
+declare var ClearBladeAsync: CbServer.ClearBladeAsync;
+declare var MQTT: CbServer.MQTT;
+/* eslint-enable no-var */
--- index.d.ts
+++ index.d.ts
@@ -616,9 +616,3 @@ declare namespace CbServer {
     runlock(): Promise<unknown>;
   }
 }
-
-/* eslint-disable no-var */
-declare var ClearBlade: CbServer.ClearBladeGlobal;
-declare var ClearBladeAsync: CbServer.ClearBladeAsync;
-declare var MQTT: CbServer.MQTT;
-/* eslint-enable no-var */
