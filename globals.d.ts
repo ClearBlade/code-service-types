948ec144aaf08552a5ec713c4f39524384087f61
Clark Bynum <cbynum@clearblade.com>
2021-09-29 15:47:45 -0500

    add 'Requests' to globals

--- globals.d.ts
+++ globals.d.ts
@@ -4,4 +4,22 @@ declare var ClearBladeAsync: CbServer.ClearBladeAsync;
 declare var MQTT: CbServer.MQTT;
 type Logger = (message?: unknown, ...args: unknown[]) => void;
 declare var log: Logger;
+declare var Requests: () => {
+  put: (
+    options: { [key: string]: unknown },
+    callback: (data: unknown) => void
+  ) => void;
+  get: (
+    options: Record<string, unknown>,
+    callback: (err: unknown, data: string) => void
+  ) => void;
+  post: (
+    options: Record<string, unknown>,
+    callback: (err: unknown, data: string) => void
+  ) => void;
+  delete: (
+    options: Record<string, unknown>,
+    callback: (err: unknown, data: string) => void
+  ) => void;
+};
 /* eslint-enable no-var */
